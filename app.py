f0e568e43bc1f9a2f4c2a95440d4ed10a8e6bb55
Roberto <roberto71979@hotmail.com>
2020-06-10 16:40:08 -0400

    add endpoint for updating config

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # API server for controlling settings
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from utils import utils
 import json
@@ -16,6 +16,23 @@ def index():
     return render_template('index.html', config=config)
 
 # POST route for updating data
+@app.route("/config", methods=['POST'])
+def config():
+    try:
+        ip = request.form.get("ip")
+        light = request.form.get("light")
+        # Get current config
+        with open("config.json", "r") as f:
+          config = json.load(f)
+          config["ip"] = ip
+          config["light"] = light
+          # Update json
+          utilities.write_json("config.json", config)
+          # Return HTML
+          return render_template('index.html', config=config)
+    except Exception as e:
+        print(e)
+        return render_template('index.html', config=config)
 
 # Emits sockets to front end for updating logs
 def emit_socket(message):
